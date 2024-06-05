6cdd3bef6d0644ad57b83c70f8c97800972298dc
Juliocastro007 <julio.castro.delgado@gmail.com>
2024-06-05 16:07:29 -0300

    Add files via upload

---
+++ Primer_Parcial/Domain/Boleto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_Parcial.Domain
+{
+    public abstract class Boleto
+    {
+        public double CostoEmbarque {  get; set; }
+
+        public DateTime FechaSalida { get; set; }
+
+        int Numero { get;}
+
+        int TiempoEnDias { get; set; }
+    }
+}
